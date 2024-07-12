207849b8a9a58a71929d4d1191063a7ce39ef381
Rilomilo <269797689@qq.com>
2024-07-12 14:53:33 +0800

    disable pretrain weights

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ from torchinfo import summary
 
 
 def get_model():
-    model = torchvision.models.segmentation.fcn_resnet50(weights=None)
+    model = torchvision.models.segmentation.fcn_resnet50(weights=None, weights_backbone=None)
     
     # pth = torch.load("../input/pretrain-coco-weights-pytorch/fcn_resnet50_coco-1167a1af.pth")
     # for key in ["aux_classifier.0.weight", "aux_classifier.1.weight", "aux_classifier.1.bias", "aux_classifier.1.running_mean", "aux_classifier.1.running_var", "aux_classifier.1.num_batches_tracked", "aux_classifier.4.weight", "aux_classifier.4.bias"]:
